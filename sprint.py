ee68db4177878b85172cf58d0e3f4ed1398d666f
Jaime Forero <j.e.forero.romero@gmail.com>
2017-04-04 10:34:53 -0500

    sprint

--- sprint.py
+++ sprint.py
@@ -18,50 +18,69 @@ tileid= a['TILEID']
 ra = a['RA']
 dec = a['DEC']
 
-# This region roughly corresponds to the observed region in September 2019
-min_ra = 0.0
-max_ra = 40.0
-min_dec = -20.0
-max_dec = 5.0
+side_grid = 3.0
+ra_min_list = np.linspace(0.0,360.0-3.0,360.0/3.0, dtype=int)
+dec_min_list = np.linspace(-90.0,90-3.0,180.0/3.0, dtype=int)
+print(ra_min_list)
+print(dec_min_list)
+n_tiles = len(a)
+grid_list = []
+for i in range(n_tiles):
+    ra_grid = int(ra[i] - ra[i]%side_grid)
+    dec_grid = int(dec[i] - dec[i]%side_grid)
+    if (ra_grid, dec_grid) not in grid_list:
+        grid_list.append((ra_grid,dec_grid))
 
-# estimate how many tiles are covered by the previous cuts
-ii = (ra<max_ra) & (ra>min_ra) & (dec<max_dec) & (dec>min_dec)
-tile_in = len(a[ii])
-tile_all = len(a)
-print('tiles fulllength:', tile_all)
-print('tiles ra,dec cut:', tile_in)
+print('n_tiles', n_tiles)
+print('n_grid', len(grid_list))
 
-#generate targets
-nproc = 24
-
-print('using nprocs {}'.format(nproc))
-
-from desitarget.mock.build import targets_truth
-from desispec.log import get_logger, DEBUG
-
-log = get_logger(DEBUG)
-
-config_file = 'mock_input.yaml'
-# Read parameters from yaml
-log.info('Reading configuration file {}'.format(config_file))
-with open(config_file, 'r') as pfile:
-    params = yaml.load(pfile)
-
-# Optionally read the "real" target catalog.
-realtargets_file = '/project/projectdirs/desi/target/catalogs/targets-dr3.1-0.8.1.fits'
-output_dir = "./output"
-bricksize = 0.25
-outbricksize = 0.50
-
-from astropy.io import fits
-print('Loading real targets from {}'.format(realtargets_file))
-realtargets = fits.getdata(realtargets_file)
+grid_i = 0
+for pair in grid_list:
+    grid_i = grid_i + 1 
+    min_ra = pair[0]
+    min_dec = pair[1]
+    print(min_ra, min_dec)
+    config_file = 'mock_input.yaml'
+    with open(config_file, 'r') as pfile:
+        params = yaml.load(pfile)
+    params['subset']['min_ra'] = min_ra 
+    params['subset']['max_ra'] = min_ra + side_grid
+    params['subset']['min_dec'] = min_dec 
+    params['subset']['max_dec'] = min_dec + side_grid 
+    output_dir = "./output_{}".format(grid_i)  
+    print('minra', params['subset']['min_ra'])
+    print('maxra', params['subset']['max_ra'])
+    print('mindec', params['subset']['min_dec'])
+    print('maxdec', params['subset']['max_dec'])
+    print('output_dir {}'.format(output_dir))
 
 
-# Construct Targets and Truth files
-targets_truth(params, output_dir, realtargets=realtargets, seed=42,
-              verbose=True, nproc=nproc, bricksize=bricksize,
-              outbricksize=outbricksize)
+    
+    #generate targets
+    generate_spec = True
+    if generate_spec:
+        nproc = 24
 
-log.info('All done!')
+        from desitarget.mock.build import targets_truth
+        from desispec.log import get_logger, DEBUG
+
+        log = get_logger(DEBUG)
+
+        # Optionally read the "real" target catalog.
+        realtargets_file = '/project/projectdirs/desi/target/catalogs/targets-dr3.1-0.8.1.fits'
+        
+        bricksize = 0.25
+        outbricksize = 0.50
+    
+        from astropy.io import fits
+        print('Loading real targets from {}'.format(realtargets_file))
+        realtargets = fits.getdata(realtargets_file)
+    
+    
+        # Construct Targets and Truth files
+        targets_truth(params, output_dir, realtargets=realtargets, seed=42,
+                      verbose=True, nproc=nproc, bricksize=bricksize,
+                      outbricksize=outbricksize)
+
+        log.info('All done!')
 
